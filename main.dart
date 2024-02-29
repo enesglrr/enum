c58bd39231edef711f245de83140efc14f953ea1
enesglrr <ramizenes.guler@ademceylantk.com.tr>
2024-02-29 10:54:03 +0300

    proje1

---
+++ main.dart
@@ -0,0 +1,7 @@
+import 'contructor.dart';
+
+void main() {
+  Calisan c1 = new Calisan("Ahmet", "Yılmaz", Departman.Yazilimci);
+  Calisan c2 = new Calisan("Mehmet", "Yıldız", Departman.Pazarlamaci);
+  print(c1.departman);
+}
